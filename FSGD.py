2db2e6baf9eb5c4a1803d32e60ec635943d372e1
Yuanpeng Li <l.yuanpeng@wustl.edu>
2024-05-04 22:15:07 -0500

    Change function name

--- FSGD.py
+++ FSGD.py
@@ -38,7 +38,7 @@ def decode_predictions(prediction):
     predicted_class_index = np.argmax(prediction)
     return class_labels[str(predicted_class_index)], predicted_class_index
 
-def create_adversarial_pattern(input_image, input_label):
+def computePerturbation(input_image, input_label):
     """Create adversarial perturbation based on model predictions."""
     with tf.GradientTape() as tape:
         tape.watch(input_image)
@@ -78,15 +78,15 @@ def main():
         label = tf.one_hot(label_index, image_probs.shape[-1])
         label = tf.reshape(label, (1, image_probs.shape[-1]))
 
-        perturbations = create_adversarial_pattern(image, label)
+        perturbations = computePerturbation(image, label)
         plt.imshow(perturbations[0] * 0.5 + 0.5);
         epsilons = [0, 0.01, 0.1, 0.15]
         descriptions = ['Input'] + [f'Epsilon = {eps:.3f}' for eps in epsilons[1:]]
 
         for eps, desc in zip(epsilons, descriptions):
-            adv_x = image + eps * perturbations
-            adv_x = tf.clip_by_value(adv_x, -1, 1)
-            display_image(adv_x, desc)
+            perturbed_image = image + eps * perturbations
+            perturbed_image = tf.clip_by_value(perturbed_image, -1, 1)
+            display_image(perturbed_image, desc)
             time.sleep(3)
 
 if __name__ == '__main__':
